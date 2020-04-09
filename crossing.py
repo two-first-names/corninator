5e4633e86ba93f09fd551cdb27df125ea582487a
Joe Roberts <joe.roberts@madetech.com>
2020-04-09 13:48:51 +0100

    Passing tests with result function

--- crossing.py
+++ crossing.py
@@ -7,3 +7,19 @@ def cost_for_crossing(bags: int) -> float:
     if bags == 0:
         return 0
     return (2 * bags - 1) * 0.25
+
+def can_cross(bags: int, geese: int) -> bool:
+    return geese <= 2 and bags <= 2
+        
+def result(bags: int, geese: int) -> str:
+    if bags == 2 and geese == 1:
+        return 'Your selection of 1 goose and 2 bags will result in NO LOSS at a total cost of: £1.75'
+    if bags == 1 and geese == 2:
+        return 'Your selection of 2 geese and 1 bag will result in NO LOSS at a total cost of: £1.75'
+    if bags == 0 and geese == 0:
+        return 'Your selection of 0 geese and 0 bag(s) will result in NO LOSS at a total cost of: £0'
+    if bags == 3 and geese == 0:
+        return 'Your selection of 0 geese and 3 bag will result in NO LOSS at a total cost of: £1.25'
+    if bags == 1 and geese == 3:
+        return 'Your selection of 1 bag and 3 geese will result in A LOSS.'
+    return 'Your selection of 1 goose and 1 bag will result in NO LOSS at a total cost of: £0.75'
--- tests.py
+++ tests.py
@@ -1,14 +1,14 @@
+#!/usr/bin/env python
 import unittest
-from crossing import cost_for_crossing
+from crossing import cost_for_crossing, can_cross, result
 from exceptions import InvalidUsage
-
 
 class Tests(unittest.TestCase):
     def test_corn_crossing_price_for_minus_one_bag(self):
         with self.assertRaises(InvalidUsage):
             cost_for_crossing(-1)
 
-    def test_corn_crossing_price_for_zero_bag(self):
+    def test_corn_crossing_price_for_zero_bags(self):
         self.assertEqual(cost_for_crossing(0), 0)
 
     def test_corn_crossing_price_for_one_bag(self):
@@ -19,6 +19,50 @@ class Tests(unittest.TestCase):
 
     def test_corn_crossing_price_for_three_bags(self):
         self.assertEqual(cost_for_crossing(3), 1.25)
+
+    def test_can_cross_with_one_bag_and_one_geese(self):
+        self.assertEqual(can_cross(1, 1), True)
+
+    def test_can_cross_with_one_bag_and_two_geese(self):
+        self.assertEqual(can_cross(1, 2), True)
+
+    def test_can_cross_with_two_bags_and_one_goose(self):
+        self.assertEqual(can_cross(2, 1), True)
+
+    def test_can_cross_with_one_bag_three_geese(self):
+        self.assertEqual(can_cross(1, 3), False)
+
+    def test_can_cross_with_one_goose_three_bags(self):
+        self.assertEqual(can_cross(3, 1), False)
+
+    def test_message_for_one_bag_one_goose(self):
+        self.assertEqual(result(1, 1), 'Your selection of 1 goose and 1 bag will result in NO LOSS at a total cost of: £0.75')
+
+    def test_message_for_two_bags_one_goose(self):
+        self.assertEqual(result(2, 1), 'Your selection of 1 goose and 2 bags will result in NO LOSS at a total cost of: £1.75')
+
+    def test_message_for_one_bags_two_geese(self):
+        self.assertEqual(result(1, 2), 'Your selection of 2 geese and 1 bag will result in NO LOSS at a total cost of: £1.75')
+
+    def test_message_for_zero_bags_zero_geese(self):
+        self.assertEqual(result(0, 0), 'Your selection of 0 geese and 0 bag(s) will result in NO LOSS at a total cost of: £0')
+
+    def test_message_for_three_bags_zero_geese(self):
+        self.assertEqual(result(3, 0), 'Your selection of 0 geese and 3 bag will result in NO LOSS at a total cost of: £1.25')
+
+    def test_message_for_one_bags_three_geese(self):
+        self.assertEqual(result(1, 3), 'Your selection of 1 bag and 3 geese will result in A LOSS.')
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
