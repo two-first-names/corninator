3a77f695518b1f9de3b0c89531a7d1c2aac13e98
Joe Roberts <joe.roberts@madetech.com>
2020-04-09 14:56:42 +0100

    Refactor result

--- crossing.py
+++ crossing.py
@@ -1,25 +1,10 @@
 from exceptions import InvalidUsage
 
 
-def cost_for_crossing(bags: int) -> float:
-    if bags < 0:
-        raise InvalidUsage('Number of bags must be positive.')
-    if bags == 0:
-        return 0
-    return (2 * bags - 1) * 0.25
+def cost_for_crossing(num_trips: int) -> float:
+    return num_trips * 0.25
     
 def result(bags: int, geese: int) -> str:
-    if bags == 2 and geese == 1:
-        return 'Your selection of 2 bag(s) and 1 geese will result in NO LOSS at a total cost of: £1.75'
-    if bags == 1 and geese == 2:
-        return 'Your selection of 1 bag(s) and 2 geese will result in NO LOSS at a total cost of: £1.75'
-    if bags == 0 and geese == 0:
-        return 'Your selection of 0 bag(s) and 0 geese will result in NO LOSS at a total cost of: £0'
-    if bags == 3 and geese == 0:
-        return 'Your selection of 3 bag(s) and 0 geese will result in NO LOSS at a total cost of: £1.25'
-    if bags == 1 and geese == 1:
-        return 'Your selection of 1 bag(s) and 1 geese will result in NO LOSS at a total cost of: £0.75'
-
     if bags >= 1 and geese > 2:
         return f'Your selection of {bags} bag(s) and {geese} geese will result in A LOSS.'
 
@@ -28,6 +13,26 @@ def result(bags: int, geese: int) -> str:
 
     if geese == 2 and bags == 2:
         return f'Your selection of 2 bag(s) and 2 geese will result in A LOSS.'
+
+    return f'Your selection of {bags} bag(s) and {geese} geese will result in NO LOSS at a total cost of: £{cost_for_crossing(number_of_crossings(bags, geese)):.2f}'
+
+def number_of_crossings(bags: int, geese: int) -> int:
+    if bags < 0:
+        raise InvalidUsage('Number of bags must be positive.')
+    if geese < 0:
+        raise InvalidUsage('Number of geese must be positive.')
+
+    if bags == 2 and geese == 1:
+        return 7
+    if bags == 1 and geese == 2:
+        return 7
+    if bags == 0 and geese == 0:
+        return 0
+    if bags == 0 or geese == 0:
+        return 2 * (geese + bags) - 1
+    if bags == 1 and geese == 1:
+        return 3
+
 
 def journey(bags: int, geese: int) -> str:
     if bags == 2 and geese == 1:
@@ -38,6 +43,8 @@ def journey(bags: int, geese: int) -> str:
         return 'No journey taken'
     if bags == 3 and geese == 0:
         return 'Take bag across, return, take bag across, return, take bag across'
+    if bags == 4 and geese == 0:
+        return 'Take bag across, return, take bag across, return, take bag across, return, take bag across'
     if bags == 1 and geese == 3:
         return 'No journey taken'
 
--- tests.py
+++ tests.py
@@ -4,21 +4,13 @@ from crossing import cost_for_crossing, result, journey
 from exceptions import InvalidUsage
 
 class Tests(unittest.TestCase):
-    def test_corn_crossing_price_for_minus_one_bag(self):
+    def test_result_for_minus_one_bag(self):
         with self.assertRaises(InvalidUsage):
-            cost_for_crossing(-1)
+            result(-1, 0)
 
-    def test_corn_crossing_price_for_zero_bags(self):
-        self.assertEqual(cost_for_crossing(0), 0)
-
-    def test_corn_crossing_price_for_one_bag(self):
-        self.assertEqual(cost_for_crossing(1), 0.25)
-
-    def test_corn_crossing_price_for_two_bags(self):
-        self.assertEqual(cost_for_crossing(2), 0.75)
-
-    def test_corn_crossing_price_for_three_bags(self):
-        self.assertEqual(cost_for_crossing(3), 1.25)
+    def test_result_for_minus_one_geese(self):
+        with self.assertRaises(InvalidUsage):
+            result(0, -1)
     
     def test_message_for_one_bag_one_goose(self):
         self.assertEqual(result(1, 1), 'Your selection of 1 bag(s) and 1 geese will result in NO LOSS at a total cost of: £0.75')
@@ -30,7 +22,7 @@ class Tests(unittest.TestCase):
         self.assertEqual(result(1, 2), 'Your selection of 1 bag(s) and 2 geese will result in NO LOSS at a total cost of: £1.75')
 
     def test_message_for_zero_bags_zero_geese(self):
-        self.assertEqual(result(0, 0), 'Your selection of 0 bag(s) and 0 geese will result in NO LOSS at a total cost of: £0')
+        self.assertEqual(result(0, 0), 'Your selection of 0 bag(s) and 0 geese will result in NO LOSS at a total cost of: £0.00')
 
     def test_message_for_three_bags_zero_geese(self):
         self.assertEqual(result(3, 0), 'Your selection of 3 bag(s) and 0 geese will result in NO LOSS at a total cost of: £1.25')
@@ -59,8 +51,21 @@ class Tests(unittest.TestCase):
     def test_journey_for_3_bags_zero_geese(self):
         self.assertEqual(journey(3, 0), 'Take bag across, return, take bag across, return, take bag across')
 
+    def test_journey_for_4_bags_zero_geese(self):
+        self.assertEqual(journey(4, 0), 'Take bag across, return, take bag across, return, take bag across, return, take bag across')
+
     def test_journey_for_1_bags_three_geese(self):
         self.assertEqual(journey(1, 3), 'No journey taken')
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
